e45458f244ca610f1a294fa1e5faa87c12322157
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-27 17:21:48 +0530

    commit changes to adam.py

---
+++ source/adam.py
@@ -0,0 +1,11 @@
+# importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# creating the input array
+X=np.array([[1,0,1,0], [1,0,1,1], [0,1,0,1]])
+
+# converting the input in matrix form
+X = X.T
+print ('\n Input:')
+print(X)
